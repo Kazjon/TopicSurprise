567e5ee21bb25971887766be9a2baf58f056931d
Kazjon Grace <k.grace@uncc.edu>
2017-06-02 16:07:51 -0400

    Updated R scripts.

--- RScripts/STM_maryam_train.R
+++ RScripts/STM_maryam_train.R
@@ -5,6 +5,10 @@ lower_thresh <- 29
 upper_thresh <- 149012
 args <- paste(lower_thresh,upper_thresh,sep="_")
 load(paste(path,args,"_preprocessed.RData",sep=""))
+docbows <- as.data.frame(corpus$documents)
+write.csv(docbows, file=paste(path,args,"_docbows.csv",sep=""),row.names=F)
+vocab <- as.data.frame(corpus$vocab)
+write.csv(vocab, file=paste(path,args,"_vocab.csv",sep=""),row.names=F)
 model <- stm(corpus$documents,corpus$vocab,K=topics)
 save(model, file=paste(path,args,"_model.RData",sep=""), row.names=F)
 theta <- as.data.frame(model$theta)
