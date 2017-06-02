baf5cf6b40393e2432585aa946391b91a978c372
kazjon <kazjon@me.com>
2017-06-02 12:02:48 -0400

    Basic surprise model working.

--- evaluate_surprise.py
+++ evaluate_surprise.py
@@ -1,14 +1,45 @@
 import sys,csv
+import pandas as pd
 import cPickle as pickle
-with open(sys.argv[1],"wb") as pf:
-	vocab, doc_bow = pickle.load(pf)
-with open(sys.argv[2],"rb") as f:
-documents = []
-	reader = csv.reader(f)
-	reader.next()
-	for l in reader:
-		documents.append({"id":l[0],"title":l[1],"authors":l[2],"raw":l[5],"topics":[float(t) for t in l[6:]]})
-for i,d in enumerate(doc_bow):
-	documents[i]["bow"] = d
-	print documents[i]
+import numpy as np
+from itertools import combinations
 
+
+with open(sys.argv[1],"rb") as pf:
+	vocab = pickle.load(pf)
+with open(sys.argv[2],"rb") as pf:
+	documents = pickle.load(pf)
+
+topic_pair_surps = np.genfromtxt(sys.argv[3],delimiter=",")
+word_occs = topic_pair_surps[1,:]
+topic_pair_surps = topic_pair_surps[1:,:]
+
+surp_percentile = 5
+for d in documents:
+	d_sig_topics = [i for i,v in enumerate(d["sig_topics"]) if v]
+	if len(d_sig_topics) > 1:
+		d_topic_pairs = list(combinations(d_sig_topics,2))
+		d_topic_pair_surps = [-np.log2(topic_pair_surps[i,j]) for i,j in d_topic_pairs]
+		d["surps"] = zip(d_topic_pairs,d_topic_pair_surps)
+		d["surp"] = np.percentile([s[1] for s in d["surps"]],surp_percentile)
+	else:
+		d["surps"] = []
+		d["surp"] = float("nan")
+
+
+sorted_docs = sorted([d for d in documents if not np.isnan(d["surp"])],key=lambda x: x["surp"])
+for doc in sorted_docs[:10]:
+	print "  Title:",doc["title"]
+	print "  ID:",doc["id"]
+	print "  Surprise:",doc["surp"]
+	print "  Surprising Topic Pairs:",doc["surps"]
+	print "  Abstract:",doc["raw"]
+	print "-----"
+print "----------------------------------------------------------------------------------------------------------------"
+for doc in sorted_docs[-10:]:
+	print "  Title:",doc["title"]
+	print "  ID:",doc["id"]
+	print "  Surprise:",doc["surp"]
+	print "  Surprising Topic Pairs:",doc["surps"]
+	print "  Abstract:",doc["raw"]
+	print "-----"
--- parse_rscripts.py
+++ parse_rscripts.py
@@ -1,12 +1,43 @@
-import sys
+import sys,csv
 import cPickle as pickle
 import numpy as np
-
+import pandas as pd
 import rpy2.robjects as robjects
 
 robjects.r["load"](sys.argv[1])
 out = robjects.r["out"]
-vocab = list(out[out.names.index("vocab")])
-doc_bow = list(out[out.names.index("documents")])
-with open("vocab_and_bow.pkl","wb") as pf:
-	pickle.dump((vocab,doc_bow),pf)
+vocab = out[out.names.index("vocab")]
+with open("vocab.pkl","wb") as pf:
+	pickle.dump(vocab,pf,protocol=pickle.HIGHEST_PROTOCOL)
+
+doc_bow = out[out.names.index("documents")]
+
+documents = []
+thresh = float(sys.argv[3])
+with open(sys.argv[2],"rb") as f:
+	reader = csv.reader(f)
+	reader.next()
+	for k,l in enumerate(reader):
+		topics = [float(t) for t in l[6:]]
+		documents.append({"id":l[0],"title":l[1],"authors":l[2],"raw":l[5],"topics":topics,
+						  "sig_topics":[True if t > thresh else False for t in topics]})
+for i,d in enumerate(doc_bow):
+	ld = list(d)
+	documents[i]["bow"] = dict(zip(ld[::2], ld[1::2]))
+
+with open("documents.pkl","wb") as pf:
+	pickle.dump(documents,pf,protocol=pickle.HIGHEST_PROTOCOL)
+
+
+sig_topics = pd.DataFrame([d["sig_topics"] for d in documents])
+topic_pair_surps = np.zeros([sig_topics.shape[1],sig_topics.shape[1]])
+topic_sig_occs = []
+for topic in sig_topics:
+	sig_topics_t = sig_topics.groupby(topic)
+	topic_pair_surps[topic,:] += np.array(sig_topics_t.get_group(True).sum(axis=0))
+	topic_sig_occs.append(topic_pair_surps[topic,topic])
+	topic_pair_surps[topic,:] /= topic_pair_surps[topic,topic]
+with open("topic_pair_surps.csv","wb") as f:
+	writer = csv.writer(f)
+	writer.writerow(topic_sig_occs)
+	writer.writerows([topic_pair_surps[i,:] for i in range(len(topic_sig_occs))])
